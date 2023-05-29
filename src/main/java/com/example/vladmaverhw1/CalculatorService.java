c969db2dc9d93dd46870976faba7d574f3009dc8
vladk <kotolupenko.vlad@mail.ru>
2023-05-29 14:13:49 +0500

    hw vlad

---
+++ src/main/java/com/example/vladmaverhw1/CalculatorService.java
@@ -0,0 +1,8 @@
+package com.example.vladmaverhw1;
+
+public interface CalculatorService {
+    String plus ( int num1, int num2);
+    String minus ( int num1, int num2);
+    String multiply ( int num1, int num2);
+    String divide ( int num1, int num2);
+}
