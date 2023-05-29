bd40f474ff74328ca06a2e1d111c75504f5cf64d
vladk <kotolupenko.vlad@mail.ru>
2023-05-29 13:15:09 +0500

    hw vlad

---
+++ src/main/java/com/example/vladmaverhw1/Vladmaverhw1Application.java
@@ -0,0 +1,13 @@
+package com.example.vladmaverhw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vladmaverhw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vladmaverhw1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vladmaverhw1/Vladmaverhw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vladmaverhw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vladmaverhw1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
